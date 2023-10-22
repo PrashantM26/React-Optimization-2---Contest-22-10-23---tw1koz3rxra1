61c2831842889069de28548ad8f0808b5156004c
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-22 10:46:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "../styles/App.css";
 import primeNumber from "../function";
 
-const App = () => {
+/*const App = () => {
   return <OptimizeTheOperation />;
 };
 
@@ -43,4 +43,51 @@ const OptimizeTheOperation = ({ onClick }) => {
   );
 };
 
-export default App;
+export default App;*/
+
+const App = () => {
+  return <OptimizeTheOperation />;
+};
+
+const OptimizeTheOperation = () => {
+  const [number, setNumber] = useState(10000);
+  const [prime, setPrime] = useState([]);
+
+  useMemo(() => {
+    const result = primeNumber(number);
+    setPrime(result);
+  }, [number]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newNumber = Number(event.target.num.value);
+    if (newNumber !== number) {
+      setNumber(newNumber);
+    }
+  };
+
+  return (
+    <div>
+      <br />
+      Enter the number:
+      <form onSubmit={submitHandler}>
+        <input id="num" />
+        <button id="submit" type="submit">
+          Click me
+        </button>
+      </form>
+      <br />
+      <div className="width">
+        Result of expensive operation:
+        <ul>
+          {prime.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default App
